1c466ebcf5021dca71e004a9bd2454716ef30dee
Fer.vi <fernanda.villeda@duxdiligens.onmicrosoft.com>
2023-06-06 16:16:43 -0600

    funcion para obtener el clima de boston

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,26 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import * as admin from 'firebase-admin';
+//el sdk de admin necesita ser inicializado primero.
+//se usa para obtener los documentos que queremos.
+admin.initializeApp();
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+export const getBostonWeather = onRequest((request, response) => {
+    //Obtener el snapshot del documento del clima de Boston
+  const promise = admin.firestore().doc('cities.weather/boston-ma-us').get()
+  //las promesas tienen un metodo llamado then que permite que tu codigo siga despues de 
+  //cumplirse la promesa con los resultados del trabajo que acaba de terminar.
+  promise.then(snapshot => {
+    //tomamos el snapshot y lo convertimos en un objeto de javascript.
+    const data = snapshot.data();
+    //y con eso, enviaremos su representación al cliente.
+    response.send(data);
+    //el objeto de respuesta sabe como convertir el objeto en JSON
+    //esta llamada para enviar la respuesta, termina la función.
+  })
 });

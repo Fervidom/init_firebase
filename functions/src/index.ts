a2e3afd089c9140efb78c261e3f5d0b6409ce693
Fer.vi <fernanda.villeda@duxdiligens.onmicrosoft.com>
2023-06-06 16:34:21 -0600

    agregar caso de error, catch de las promesas

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,10 +8,9 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 import * as admin from 'firebase-admin';
-//el sdk de admin necesita ser inicializado primero.
-//se usa para obtener los documentos que queremos.
+// el sdk de admin necesita ser inicializado primero.
+// se usa para obtener los documentos que queremos.
 admin.initializeApp();
 
 // Start writing functions
@@ -19,16 +18,25 @@ admin.initializeApp();
 
 
 export const getBostonWeather = onRequest((request, response) => {
-    //Obtener el snapshot del documento del clima de Boston
-  const promise = admin.firestore().doc('cities.weather/boston-ma-us').get()
-  //las promesas tienen un metodo llamado then que permite que tu codigo siga despues de 
-  //cumplirse la promesa con los resultados del trabajo que acaba de terminar.
-  promise.then(snapshot => {
-    //tomamos el snapshot y lo convertimos en un objeto de javascript.
+  // Obtener el snapshot del documento del clima de Boston
+  const promise = admin.firestore().doc("cities.weather/boston-ma-us").get();
+  // las promesas tienen un metodo llamado then, permite que tu codigo siga al
+  // cumplirse la promesa con los resultados del trabajo que acaba de terminar.
+  const p2 = promise.then((snapshot) => {
+    // tomamos el snapshot y lo convertimos en un objeto de javascript.
     const data = snapshot.data();
-    //y con eso, enviaremos su representación al cliente.
+    // y con eso, enviaremos su representación al cliente.
     response.send(data);
-    //el objeto de respuesta sabe como convertir el objeto en JSON
-    //esta llamada para enviar la respuesta, termina la función.
+    // el objeto de respuesta sabe como convertir el objeto en JSON
+    // esta llamada para enviar la respuesta, termina la función.
+    //Que pasa si esta promesa es rechazada con un error?
+
+  })// atraparemos lo que sea que salga mal con el metodo get de firestore
+  p2.catch((error) => {
+    // handle error
+    // mostramos el error
+    console.log(error);
+    // enviamos esa respuesta de error al cliente.
+    response.status(500).send(error);
   })
 });
